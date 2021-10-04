471990e4e0cbb08d8129b0bb68327a7385bbb590
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-10-04 09:25:41 -0300

    refactor TodoInput.test and todoReducer.test

--- src/components/input/TodoInput.test.jsx
+++ src/components/input/TodoInput.test.jsx
@@ -1,66 +1,51 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import {
+  render, screen, fireEvent, cleanup,
+} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import store from '../../redux/store';
 import TodoInput from './TodoInput';
 
 describe('TodoInput component', () => {
-  it('container renders correctly', () => {
-    const onClickMock = jest.fn();
+  let addClickMock;
+
+  beforeEach(() => {
+    addClickMock = jest.fn();
     render(
       <Provider store={store}>
-        <TodoInput handleAddButtonClick={onClickMock} />
+        <TodoInput handleAddButtonClick={addClickMock} />
       </Provider>,
     );
+  });
+
+  afterEach(cleanup);
+
+  it('container renders correctly', () => {
     const container = screen.getByTestId('inputContainer');
     expect(container).toBeInTheDocument();
   });
 
   it('input element renders correctly', () => {
-    const onClickMock = jest.fn();
-    render(
-      <Provider store={store}>
-        <TodoInput handleAddButtonClick={onClickMock} />
-      </Provider>,
-    );
     const input = screen.getByTestId('todoInput');
     expect(input).toBeInTheDocument();
   });
 
   it('add todo button renders correctly', () => {
-    const onClickMock = jest.fn();
-    render(
-      <Provider store={store}>
-        <TodoInput handleAddButtonClick={onClickMock} />
-      </Provider>,
-    );
     const addBtn = screen.getByTestId('addButton');
     expect(addBtn).toBeInTheDocument();
   });
 
   it('changes input element value', () => {
-    const onClickMock = jest.fn();
-    render(
-      <Provider store={store}>
-        <TodoInput handleAddButtonClick={onClickMock} />
-      </Provider>,
-    );
     const input = screen.getByTestId('todoInput');
     fireEvent.change(input, { target: { value: 'test' } });
     expect(input.value).toBe('test');
   });
 
   it('calls "onClick" on button click', () => {
-    const onClickMock = jest.fn();
-    render(
-      <Provider store={store}>
-        <TodoInput handleAddButtonClick={onClickMock} />
-      </Provider>,
-    );
     const addBtn = screen.getByTestId('addButton');
 
     fireEvent.click(addBtn);
-    expect(onClickMock).toHaveBeenCalledTimes(1);
+    expect(addClickMock).toHaveBeenCalledTimes(1);
   });
 });
--- src/redux/reducers/todoReducer.test.js
+++ src/redux/reducers/todoReducer.test.js
@@ -2,66 +2,49 @@ import todoReducer, {
   addTodo, deleteTodo, editTodo, completeTodo,
 } from './todoReducer';
 
+const testState = {
+  todoList: [
+    {
+      id: 1,
+      completed: false,
+      text: 'text 1',
+    },
+    {
+      id: 2,
+      completed: false,
+      text: 'text 2',
+    },
+    {
+      id: 3,
+      completed: false,
+      text: 'text 3',
+    },
+  ],
+};
+
 describe('todoReducer', () => {
-  it('Should return default state', () => {
+  it('Should return default state if no action is received', () => {
     const newState = todoReducer(undefined, {});
     expect(newState).toEqual({ todoList: [] });
   });
 
-  it('Should return new state if receiving action type addTodo', () => {
-    const todo = 'study redux';
+  it('Should return new state if receiving action type addTodo and todo must have id 1', () => {
     const newState = todoReducer(undefined, {
       type: addTodo,
-      payload: todo,
+      payload: 'testTodo',
     });
 
     const expectedItem = {
       completed: false,
       id: 1,
-      text: todo,
+      text: 'testTodo',
     };
 
     expect(newState).toEqual({ todoList: [expectedItem] });
   });
 
-  it('Should create todo with id 1 in case there are not previous todos', () => {
-    const todo = 'test';
-    const newState = todoReducer(undefined, {
-      type: addTodo,
-      payload: todo,
-    });
-
-    const expectedNewState = {
-      completed: false,
-      id: 1,
-      text: todo,
-    };
-
-    expect(newState).toEqual({ todoList: [expectedNewState] });
-  });
-
   it('Should create todo with id 4 in case there are two previous todos', () => {
-    const initialState = {
-      todoList: [
-        {
-          id: 1,
-          completed: false,
-          text: 'text 1',
-        },
-        {
-          id: 2,
-          completed: false,
-          text: 'text 2',
-        },
-        {
-          id: 3,
-          completed: false,
-          text: 'text 3',
-        },
-      ],
-    };
-
-    const newState = todoReducer(initialState, {
+    const newState = todoReducer(testState, {
       type: addTodo,
       payload: 'text 4',
     });
@@ -95,30 +78,14 @@ describe('todoReducer', () => {
   });
 
   it('Should delete todo if receiving action type deleteTodo', () => {
-    const initialState = {
-      todoList: [{
-        id: 1,
-        completed: false,
-        text: 'study redux',
-      }],
-    };
     const idToBeRemoved = 1;
-    const newState = todoReducer(initialState, {
+    const newState = todoReducer(testState, {
       type: deleteTodo,
       payload: idToBeRemoved,
     });
 
-    expect(newState).toEqual({ todoList: [] });
-  });
-
-  it('Should edit todo if receiving action type editTodo', () => {
-    const initialState = {
+    const expectedNewState = {
       todoList: [
-        {
-          id: 1,
-          completed: false,
-          text: 'text 1',
-        },
         {
           id: 2,
           completed: false,
@@ -131,9 +98,13 @@ describe('todoReducer', () => {
         },
       ],
     };
+    expect(newState).toEqual(expectedNewState);
+  });
+
+  it('Should edit todo if receiving action type editTodo', () => {
     const idToBeEdited = 2;
 
-    const newState = todoReducer(initialState, {
+    const newState = todoReducer(testState, {
       type: editTodo,
       payload: {
         id: idToBeEdited,
@@ -165,26 +136,7 @@ describe('todoReducer', () => {
   });
 
   it('Should mark checkbox as checked if receiving action type completeTodo', () => {
-    const initialState = {
-      todoList: [
-        {
-          id: 1,
-          completed: false,
-          text: 'test 1',
-        },
-        {
-          id: 2,
-          completed: false,
-          text: 'test 2',
-        },
-        {
-          id: 3,
-          completed: false,
-          text: 'test 3',
-        },
-      ],
-    };
-    const newState = todoReducer(initialState, {
+    const newState = todoReducer(testState, {
       type: completeTodo,
       payload: {
         id: 1,
@@ -197,17 +149,17 @@ describe('todoReducer', () => {
         {
           id: 1,
           completed: true,
-          text: 'test 1',
+          text: 'text 1',
         },
         {
           id: 2,
           completed: false,
-          text: 'test 2',
+          text: 'text 2',
         },
         {
           id: 3,
           completed: false,
-          text: 'test 3',
+          text: 'text 3',
         },
       ],
     };
